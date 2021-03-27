7ac4ea677b218d24c13291963742034ed1651ddd
Sirawit <popeye-zz@hotmail.com>
2021-03-28 02:18:18 +0700

    finish all frontend -> next is mockup backend

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AddTask from './components/AddTask'
 
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
 
   const [tasks, setTasks] = useState([
     {
@@ -51,8 +52,12 @@ const App = () => {
   return (
     <div className="container">
       
-      <Header title='Task Tracker'/>
-      <AddTask onAdd={addTask}/>
+      <Header 
+        title='Task Tracker' 
+        onAdd={() => setShowAddTask(!showAddTask)} 
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask}/>} 
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks to Show'}
 
       
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,19 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 //rafce
-const Header = ({title}) => {
+const Header = ({title, onAdd, showAdd}) => {
     
-    const onClick = () => {
-        console.log('Click')
-    }
-    
+
     return (
         <header className='header'>
             
             {/*<h1 style={headingStyle}>{title}</h1>*/}
             <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick}/>
+            <Button 
+                color={showAdd ? 'darkgreen':'green'}
+                text={showAdd ? 'Close': 'Add'} 
+                onClick={onAdd}
+            />
         
         </header>
     )
